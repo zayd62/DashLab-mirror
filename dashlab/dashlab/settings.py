d5fac11568f4cf5ab517c1b9f819e1c9f692b7d3
Vlad <vladgavinas@gmail.com>
2019-03-17 22:22:11 +0000

    Working authentication for one user account type
    User authentication without knox that allows a user to make an account and login, displaying the home page.

--- dashlab/dashlab/settings.py
+++ dashlab/dashlab/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'post'
+    'post',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'dashlab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- dashlab/dashlab/urls.py
+++ dashlab/dashlab/urls.py
@@ -1,7 +1,13 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import home
+
+from accounts.views import login_view, register_view, logout_view
 
 urlpatterns = [
-    path('', include('post.urls')),
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('accounts/login/', login_view),
+    path('accounts/register/', register_view),
+    path('accounts/logout/', logout_view)
 ]
