8cec9c7ce1a5816b2460f70100ee166e52f01d48
Vlad <vladgavinas@gmail.com>
2019-03-23 18:57:08 +0000

    Added more URLs

--- src/APIURLS.js
+++ src/APIURLS.js
@@ -1,11 +1,11 @@
 // see post for example
 export const domain = "dashlab.com";
 // be careful of what is returned, either a json object or an array of json objects
-export const createuser = `${domain}/auth/users/create`; //post
-export const login = `${domain}/auth/token/login`; // post
-export const logout = `${domain}/auth/token/logout`; // post (require token of user you want to logout)
-export const listAllUsers = `${domain}/auth/users/`; // post (require token of user)
-
+export const createuser = `${domain}/auth/users/create`; //post request
+export const login = `${domain}/auth/token/login`; // post request
+export const logout = `${domain}/auth/token/logout`; // post request (require token of user you want to logout)
+export const listAllUsers = `${domain}/auth/users/`; // post request(require token of user)
+export const listCurrentUser = `${domain}/auth/users/me/` // get request
 export const getSocialMediaAccount = `${domain}/api/socialmediaaccount/` 
 export const getPost = `${domain}/api/post/` 
 export const getProfile = `${domain}/api/profile/` 
