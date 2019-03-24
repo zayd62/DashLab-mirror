57ccfa90f9f6ad999501e58f8ce64d47eafe342e
Zayd Patel <zayd62@gmail.com>
2019-03-24 01:11:12 +0000

    A whole bunch of changes
    - cleaned up imports
    - error checking to make sure that all fields are present
    - removed username textbox as that is the same as email address
    - changed password texfield to show password as admin needs to be able to set a password
    - removed password2 text field
    - fixed submit button functionality

--- src/Registration.js
+++ src/Registration.js
@@ -1,41 +1,45 @@
 import React, { Component, Fragment } from "react";
 import {
-  Dialog,
-  DialogActions,
   RadioGroup,
   FormControlLabel,
   Radio,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
   Typography,
   Button,
-  Grid,
-  InputLabel,
-  Input,
-  NativeSelect,
-  FormControl,
-  FormHelperText,
   TextField,
   AppBar,
-  Toolbar
+  Toolbar,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
 } from "@material-ui/core";
 import "./Registration.css";
+import { FUNC_CREATEUSERMODEL, FUNC_CREATEUSERMODELTWO } from "./api";
 export class Registration extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      role: "",
       email: "",
       fname: "",
       lname: "",
-      username: "",
       password: "",
-      password2: "",
-      errorText: ""
+      errorText: "",
+      dialogIsOpen: false, //used to control the dialog box because 
     };
   }
+
+  // for dialog box
+  handleClickOpen = () => {
+    this.setState({ dialogIsOpen: true });
+  };
+
+  // for dialog box
+  handleClose = () => {
+    this.setState({ dialogIsOpen: false });
+  };
+
 
   validateForm() {
     return this.state.email.length > 0 && this.state.password.length > 0;
@@ -45,6 +49,41 @@ export class Registration extends Component {
     // handles the change in the value of the dropdown
     this.setState({ [name]: event.target.value });
   };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    console.log("in handlesubmit, checking for empty fields")
+
+    // check if any field are empty. if they are, call return to stop execution of handle submit
+    if (!(this.state.email && this.state.fname && this.state.lname && this.state.password)) {
+      // if it goes here, that means that any fields are empty
+      this.setState({ errorText: "Missing fields" })
+      console.log("found missing fields")
+      this.setState({ dialogIsOpen: true })
+      return
+    }
+    console.log("no missing fields yay, creating a user model")
+
+    // making post request to api server to create user model
+    // the headers for creating a post
+    var config = {
+      headers: { "Content-Type": "application/json" }
+    }
+
+    // the body containing the email, username and password 
+    var body = {
+      email: this.state.email,
+      username: this.state.email,
+      password: this.state.password
+    }
+
+    FUNC_CREATEUSERMODEL()
+    FUNC_CREATEUSERMODELTWO()
+
+
+
+  }
 
   render() {
     return (
@@ -56,30 +95,33 @@ export class Registration extends Component {
             </Typography>
           </Toolbar>
         </AppBar>
+
         <div className="Backbox2">
           <div className="Title1">
             <h1>Create Account</h1>
           </div>
-          <form className="Regform">
+
+          <form className="Regform" onSubmit={this.handleSubmit}>
             <RadioGroup
               className="Role"
               aria-label="Role"
               name="Role"
               value={this.state.value}
-              onChange={this.handleChange}
+              onChange={this.handleChange('value')}
             >
               <FormControlLabel
-                value="JMM"
+                value="false"
                 control={<Radio />}
                 label="Junior Media Manager"
               />
               <FormControlLabel
-                value="Administrator"
+                value="true"
                 control={<Radio />}
                 label="Administrator"
               />
               />
             </RadioGroup>
+
             <div className="data">
               <TextField
                 className="fname"
@@ -92,6 +134,7 @@ export class Registration extends Component {
                 onChange={this.handleChange("fname")}
                 value={this.state.fname}
               />
+
               <TextField
                 className="lname"
                 id="lname"
@@ -103,6 +146,7 @@ export class Registration extends Component {
                 onChange={this.handleChange("lname")}
                 value={this.state.lname}
               />
+
               <TextField
                 className="email"
                 id="email"
@@ -114,52 +158,42 @@ export class Registration extends Component {
                 onChange={this.handleChange("email")}
                 value={this.state.email}
               />
-              <TextField
-                className="username"
-                id="username"
-                label="Username"
-                type="username"
-                autoComplete="username"
-                margin="normal"
-                variant="outlined"
-                onChange={this.handleChange("username")}
-                value={this.state.username}
-              />
+
               <TextField
                 className="pass1"
                 id="password"
                 label="Password"
-                type="password"
+                type="text"
                 autoComplete="password"
                 margin="normal"
                 variant="outlined"
                 onChange={this.handleChange("password")}
                 value={this.state.password}
               />
-              <TextField
-                className="pass2"
-                id="password2"
-                label="Repeat Password"
-                type="password"
-                autoComplete="password"
-                margin="normal"
-                variant="outlined"
-                onChange={this.handleChange("password2")}
-                value={this.state.password2}
-              />
             </div>
-            <Button
-              onClick={() => {
-                this.handleClick();
-              }}
-              className="regbut"
-              color="primary"
-              variant="contained"
-              type="submit"
-            >
+
+            <Button className="regbut" color="primary" variant="contained" type="submit">
               Submit
             </Button>
           </form>
+        </div>
+
+        <div>
+          <Dialog open={this.state.dialogIsOpen} onClose={this.handleClose}>
+            <DialogTitle id="alert-dialog-title">{"Error"}</DialogTitle>
+
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {this.state.errorText}
+              </DialogContentText>
+            </DialogContent>
+
+            <DialogActions>
+              <Button onClick={this.handleClose} color="primary" autoFocus>
+                Ok
+                        </Button>
+            </DialogActions>
+          </Dialog>
         </div>
       </Fragment>
     );
